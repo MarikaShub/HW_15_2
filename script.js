ddca526cdf4685f57bd0e7318faecad58451f47f
MarikaShub <city-sma@mail.ru>
2024-06-30 10:49:48 +0300

    change js

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-let cities = ['Ставрополь', 'Михайловск', 'Краснодар', 'Ростов'];
+let cities = ["Ставрополь", "Михайловск", "Краснодар", "Ростов"];
 let temperaturesInCities = [];
 for (let i = 0; i < cities.length; i++) {
-    let temperature = prompt(`Введите температуру в городе ${cities[i]} в Цельсиях`);
-    temperaturesInCities.push(Number(temperature));
+  let temperature = prompt(
+    `Введите температуру в городе ${cities[i]} в Цельсиях`
+  );
+  temperaturesInCities.push(Number(temperature));
 }
 
-
-for (let i=0; i<temperaturesInCities.length; i++) {
-    const temperatureList = document.querySelector('.temperature');
-const temperatureItem = document.createElement('li');
-temperatureItem.textContent = `Температура в городe ${cities[i]}: ${temperaturesInCities[i]} °С`;
-temperatureList.append(temperatureItem);
+for (let i = 0; i < temperaturesInCities.length; i++) {
+  const temperatureList = document.querySelector(".temperature");
+  const temperatureItem = document.createElement("li");
+  temperatureItem.textContent = `Температура в городe ${cities[i]}: ${temperaturesInCities[i]} °С`;
+  temperatureList.append(temperatureItem);
 }
-
 
 let higestTemperature = Math.max(...temperaturesInCities);
 
-let maxTemperature = document.querySelector('.maxtemperature');
+let maxTemperature = document.querySelector(".maxtemperature");
 maxTemperature.textContent = `Максимальная температура: ${higestTemperature} °С`;
 
 let lowestTemperature = Math.min(...temperaturesInCities);
-let minTemperature = document.querySelector('.mintemperature');
+let minTemperature = document.querySelector(".mintemperature");
 minTemperature.textContent = `Минимальная температура: ${lowestTemperature} °С`;
